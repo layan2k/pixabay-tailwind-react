d7ec4cd76989cacba5ce92f94bbd1072d91bf53a
layan2k <lesliedeeshumba@gmail.com>
2021-08-02 18:57:02 +0200

    project init

--- README.md
+++ README.md
@@ -68,3 +68,10 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+### Pixabay Project Details
+
+Api in use available in Pixabay
+Styling framework used === Tailwind
+
+This project was done in line with learning the React Basics
--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react';
+import Imagecard from './components/imagecard';
+import ImageSearch from './components/imageSearch';
+
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setisLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  useEffect(
+    () => {
+    fetch(`https://pixabay.com/api/?key=22753044-11eaf2d9328b7df04e81d83ad&q=${term}&image_type=photo`)
+    .then((res) => res.json())
+    .then(data => {
+      setImages(data.hits);
+      setisLoading(false)
+    })
+    .then(err => console.log(err));
+    }, [term]
+    );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto">
+      <ImageSearch searchText={(text) => setTerm(text)} />
+      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1> }
+    {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> :
+      <div className="grid grid-cols-3 gap-4">
+        {images.map(image => (
+          <Imagecard key={image.id} image={image} />
+        ))}
+      </div>}
     </div>
+
+
   );
 }
 
